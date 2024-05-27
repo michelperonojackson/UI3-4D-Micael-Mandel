69ed04aced21855395615576a2ac82c47ec1e288
michelperonojackson <163850735+michelperonojackson@users.noreply.github.com>
2024-05-27 09:04:12 -0300

    Update LoginManager.cs

--- Assets/LoginManager.cs
+++ Assets/LoginManager.cs
@@ -1,18 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LoginManager : MonoBehaviour
 {
+    public string password = "contraseña";
+    public InputField inputPassword;
+    
+    public void Saludar()
+    {
+       
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (inputPassword.text == password)
+        {
+            Debug.Log("Accsess granted");
+            
+        }
+        else
+        {
+            Debug.Log("Accsess denied");
+        }
     }
 }
